7fc9460b88465919627ed9b0ef98474b3529939d
Andrés Martínez <autummata@gmail.com>
2025-09-14 13:59:53 -0700

    update: iife desc

--- iife/index.js
+++ iife/index.js
@@ -1,13 +1,16 @@
 // IIFE (Inmediately Invoked Function Expression) is an idiom in which a JS func runs as soon as it is defined. 
+// They tend to be quite useful for maintaining local scope variables 
 
 (function () {
     console.log(":D");
 })();
 
+// You can use arrow functions 
 (() => {
     console.log("D:");
 })();
 
+// Even async works too
 (async () => {
     console.log("... :D!");
 })();
